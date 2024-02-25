bc0b69b8b7dd8fb4047f6093cd77c7ddad881903
Bernard De Guzman <deguzmanbernardandrew@gmail.com>
2024-02-25 21:44:41 +0800

    Remove unneeded code

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -10,13 +10,6 @@
                         100characters
                     </a>
                 </div>
-
-                <!-- Navigation Links -->
-                {{-- <div class="hidden space-x-8 sm:-my-px sm:ms-10 zsm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
-                </div> --}}
             </div>
 
             <!-- Settings Dropdown -->
@@ -91,12 +84,6 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{ 'block': open, 'hidden': !open }" class="hidden sm:hidden">
-        {{-- <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
-        </div> --}}
-
         <!-- Responsive Settings Options -->
         <div class="flex flex-col gap-4 px-5 pt-4 pb-5 border-t border-gray-200 dark:border-gray-600">
             <div class="items-center sm:flex sm:items-center sm:ms-6 dark:text-white">
