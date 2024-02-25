9a9df0fbf86d427d7c5ed3a7887575ccb3c47f68
Bernard De Guzman <deguzmanbernardandrew@gmail.com>
2024-02-25 21:26:35 +0800

    Allow mixed content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,10 +1,19 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<<<<<<< Updated upstream
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+=======
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
+        <title>{{ config('app.name', 'Laravel') }}</title>
+>>>>>>> Stashed changes
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
